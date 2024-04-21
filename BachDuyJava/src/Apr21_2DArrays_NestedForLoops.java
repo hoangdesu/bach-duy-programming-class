36589e92867cdae9baece3dae1d7eda1aa98e889
hoangdesu <hoangdesu@gmail.com>
2024-04-21 16:41:44 +0700

    2D arrays, nested for loops

---
+++ BachDuyJava/src/Apr21_2DArrays_NestedForLoops.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Apr21_2DArrays_NestedForLoops {
+    public static void main(String[] args) {
+        int[] row1 = {1, 2, 3};
+        int[] row2 = {4, 5, 6};
+        int[] row3 = {7, 8, 9};
+
+        int[][] phonePad = {row1, row2, row3};
+
+//        System.out.println(Arrays.toString(phonePad));
+
+//        System.out.println(phonePad[0][1]);
+
+//        for (int[] row : phonePad) {
+//            for (int num : row) {
+//                System.out.print(num + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i < phonePad.length; i++) {
+//            System.out.print("Row " + (i+1) + ": ");
+//            for (int j = 0; j < phonePad[i].length; j++) {
+//                System.out.print(phonePad[i][j]);
+////                System.out.printf(": i = %d, j = %d, num = %d\n", i, j, phonePad[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+//        for (int min = 0; min < 2; min++) {
+//            for (int second = 0; second < 60; second++) {
+//                System.out.printf("%d:%d\n", min, second);
+//            }
+//        }
+
+
+//        Ex:
+//        Enter number of lines:
+//
+//        l1:
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+//
+//        lv2:
+//        1
+//        12
+//        123
+//        1234
+//        12345
+
+//        lv3: draw a christmas tree
+//        Enter number of lines for your Christmas tree: 5
+//        Enter character: *
+
+//            *
+//           ***
+//          *****
+//         *******
+//        *********
+//           |_|
+
+    }
+
+
+}
