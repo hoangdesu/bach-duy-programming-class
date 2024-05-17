a1d8ef0cb5e60ef2ac020032fc9d659df4253d89
hoangdesu <hoangdesu@gmail.com>
2024-05-18 00:51:07 +0700

    word frequency

---
+++ BachDuyJava/src/May17_Methods.java
@@ -0,0 +1,92 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class May17_Methods {
+
+    public static void main(String[] args) {
+
+//        String[] cities = {"Ho Chi Minh city", "Ha Noi"};
+//
+//        System.out.println(cities[1]);
+//
+//        HashMap<String, String> duy = new HashMap<>();
+//
+//        duy.put("name", "Bach Duy");
+//        duy.put("birthYear", "2004");
+//
+//        System.out.println(duy.get("birthYear"));
+
+//        1. ArrayList
+//        2. Hashmap
+
+        String text = "hi hey hello Hello hi hey hey hi";
+        text = "It acts as a single entry point, hiding all the internal implementation details and promoting easier interaction with the underlying functionalities. It acts as a gateway, shielding clients from intricate details.";
+        text = "You might have a Node.js application that interacts with multiple external services, each requiring specific configuration parameters. Using the Singleton pattern, you can create a ConfigManager class responsible for handling these configurations in a centralized manner.";
+
+        ArrayList<String[]> wordFrequency = getWordFrequency(text);
+
+        for (String[] entry : wordFrequency) {
+//            System.out.println(Arrays.toString(entry));
+            System.out.print(entry[0] + ": " + entry[1]);
+            System.out.println();
+        }
+//
+//        [
+//                {"hey", "1"},
+//                {"hello", "2"},{
+//                {"hi", 1}
+//        }
+
+
+//        ]
+
+//        ArrayList<String> wordFrequency = getWordFrequency(text);
+//        System.out.println(wordFrequency);
+
+//        [
+//                word: frequency
+//        ]
+//
+//        [optional]: sort the array by frequency
+
+    }
+
+    public static ArrayList<String[]> getWordFrequency(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[-+.^:,]",""); // remove all special characters from the string
+
+        String[] words = text.split(" ");
+
+        ArrayList<String[]> wordFrequency = new ArrayList<>();
+
+        // go through each word in the sentence
+        for (String word : words) {
+            int index = -1; // -1 means word does not exist
+
+            for (int i = 0; i < wordFrequency.size(); i++) {
+                if (word.equals(wordFrequency.get(i)[0])) {
+                    index = i;
+                    break;
+                }
+            }
+
+            // create a new entry if the word does not exist in our data yet
+            if (index == -1) {
+                String[] entry = new String[2];
+                entry[0] = word;
+                entry[1] = "1";
+                wordFrequency.add(entry);
+            } else {
+                // otherwise update the frequency counter for such word
+                String[] entry = wordFrequency.get(index);
+                int counter = Integer.parseInt(entry[1]);
+                counter++;
+                entry[1] = String.valueOf(counter);
+                wordFrequency.set(index, entry);
+            }
+        }
+
+        return wordFrequency;
+    }
+}
