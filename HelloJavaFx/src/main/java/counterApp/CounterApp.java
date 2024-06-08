81f8972b861eec97498511d0e7e6fad90b9a5523
hoangdesu <hoangdesu@gmail.com>
2024-06-09 00:30:19 +0700

    javafx, binding actions, lambda

--- HelloJavaFx/src/main/java/counterApp/CounterApp.java
+++ HelloJavaFx/src/main/java/counterApp/CounterApp.java
@@ -1,16 +1,66 @@
 package counterApp;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 //MVC pattern
 public class CounterApp extends Application {
+
+    private static int counter = 1;
+
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(CounterApp.class.getResource("counter-app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        Button btnClickMe = new Button();
+        btnClickMe.setText(counter + "");
+        // lambda
+        btnClickMe.setOnMouseClicked(mouseEvent -> {
+//            System.out.println("Click click!!!");
+            btnClickMe.setText(++counter + "");
+            System.out.println(mouseEvent.getClickCount());
+//            System.out.println(mouseEvent.);
+            System.out.println(mouseEvent.getX() + ":" + mouseEvent.getY());
+            if (mouseEvent.getX() < 200) {
+                btnClickMe.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(0), Insets.EMPTY)));
+            } else {
+                btnClickMe.setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(0), Insets.EMPTY)));
+            }
+        });
+
+
+        EventHandler<ActionEvent> onBtnClick = event -> {
+//                System.out.println("Hello World!");
+            System.out.println("Clicked on btn");
+
+        };
+
+//        same shit
+//        EventHandler<ActionEvent> onBtnClick = new EventHandler<>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+////                System.out.println("Hello World!");
+//                System.out.println("Clicked on btn");
+//
+//            }
+//        };
+
+        btnClickMe.setOnAction(onBtnClick);
+
+
+        Scene btnScene = new Scene(btnClickMe, 500, 500);
+
         stage.setTitle("Counter app");
         stage.setScene(scene);
         stage.show();
--- HelloJavaFx/src/main/java/counterApp/CounterController.java
+++ HelloJavaFx/src/main/java/counterApp/CounterController.java
@@ -5,6 +5,9 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.paint.Color;
 
 public class CounterController {
     @FXML
@@ -15,6 +18,9 @@ public class CounterController {
 
     @FXML
     Label lbValue;
+
+    @FXML
+    Label error;
 
     // JS
 //    const btnIncrease = document.querySelector("#btnIncrease");
@@ -27,14 +33,23 @@ public class CounterController {
 //    2 ways to bind event listeners
 
     public void increase() {
+        if (this.value == 0) {
+            error.setText("");
+        }
         this.value++;
         lbValue.setText(this.value + ""); // remember to IMPERATIVELY set the UI change
         System.out.println(this.value);
     }
 
     public void decrease() {
-        this.value--;
-        lbValue.setText(this.value + "");
+        if (this.value == 0) {
+            error.setTextFill(Color.RED);
+            error.setText("Error: cannot go negative");
+        } else {
+            this.value--;
+            lbValue.setText(this.value + "");
+        }
+
         System.out.println(this.value);
     }
 
