67db77272aca68601f6a3815d9cd1fbc07fdd5c8
hoangdesu <hoangdesu@gmail.com>
2024-07-30 11:52:46 +0700

    added readme for lol-client javafx + created new Web module

---
+++ League-client-JavaFX/README.md
@@ -0,0 +1,7 @@
+# League of Legends client in JavaFX
+
+Home screen
+![Home screen](images/home.jpg)
+
+Champion view screen
+![Champion view screen](images/zed.jpg)
---
+++ Web/jul29_html/demo.js
@@ -0,0 +1,21 @@
+// @FXML id
+
+const button = document.querySelector('#click-me');
+const h6 = document.querySelector('h6');
+let isOn = false;
+
+button.addEventListener('click', () => {
+    // alert('sup bro!')
+    
+    // toggle
+    isOn = !isOn;
+    
+    if (isOn) {
+        h6.style.color = 'purple';
+        h6.textContent = 'sup bro!';
+    } else {
+        h6.style.color = 'pink';
+        h6.textContent = 'hehe';
+    }
+
+})
