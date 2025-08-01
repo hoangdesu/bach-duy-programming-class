ac17d7645c063103070ee966aefe352090f3b9fb
hoangdesu <hoangdesu@gmail.com>
2025-08-01 18:09:49 +0700

    child state, controlled vs uncontrolled components, search input

--- todo-app/todo-react/src/Rophim.jsx
+++ todo-app/todo-react/src/Rophim.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react"
 import MovieCard from "./components/MovieCard"
 
-const movies = [
+const movieData = [
     {
         poster: 'https://static.nutscdn.com/vimg/400-0/6851d117d10b27330999205e42568126.webp',
         title: 'The Fantastic Four: First Steps',
@@ -21,22 +22,17 @@ const movies = [
         title: 'Bí Kíp Luyện Rồng',
         genres: ['Chiếu Rạp', 'Siêu Anh Hùng']
     },
-        {
+    {
         poster: 'https://static.nutscdn.com/vimg/400-0/58193a004ba1bf56b271215bd51c093c.jpg',
         title: 'Bí Kíp Luyện Rồng',
         genres: ['Chiếu Rạp', 'Siêu Anh Hùng']
     },
-        {
+    {
         poster: 'https://static.nutscdn.com/vimg/400-0/58193a004ba1bf56b271215bd51c093c.jpg',
         title: 'Bí Kíp Luyện Rồng',
         genres: ['Chiếu Rạp', 'Siêu Anh Hùng']
     },
-        {
-        poster: 'https://static.nutscdn.com/vimg/400-0/58193a004ba1bf56b271215bd51c093c.jpg',
-        title: 'Bí Kíp Luyện Rồng',
-        genres: ['Chiếu Rạp', 'Siêu Anh Hùng']
-    },
-        {
+    {
         poster: 'https://static.nutscdn.com/vimg/400-0/58193a004ba1bf56b271215bd51c093c.jpg',
         title: 'Bí Kíp Luyện Rồng',
         genres: ['Chiếu Rạp', 'Siêu Anh Hùng']
@@ -44,15 +40,71 @@ const movies = [
 ]
 
 export default function Rophim() {
-    return (
-        <div style={{ display: 'flex', gap: '20px', overflow: 'scroll', width: '100%' }}>
-            {movies.map(mov => (
-                <MovieCard 
-                    movie={mov}
-                    key={Math.random()}
-                />
-            ))}
-        </div>
+    const [movies, setMovies] = useState(movieData);
 
+    const [searchQuery, setSearchQuery] = useState('');
+
+
+    const onDeleteHandler = (index) => {
+        console.log('deleting from parent', movies[index].title);
+        
+        const updatedMovies = [...movies];
+
+        updatedMovies.splice(index, 1);
+
+        setMovies(updatedMovies);
+    }
+
+    // Controlled vs Uncontrolled Components
+    
+    const searchHandler = (evt) => {
+        console.log(evt.target.value);
+
+        // Two-way binding
+        setSearchQuery(evt.target.value);
+        
+        // Revert the list to og
+        if (evt.target.value === '') {
+            setMovies(movieData);
+            return;
+        }
+
+        // Filter the og list
+        const filteredMovies = movieData.filter(m => m.title.toLowerCase().includes(evt.target.value));
+
+        setMovies(filteredMovies);
+    }
+
+    const resetSearchHandler = () => {
+        setSearchQuery('');
+        setMovies(movieData);
+    }
+
+    return (
+        <div>
+            <div style={{ width: '100%', textAlign: 'center', position: 'relative' }}>
+                <input 
+                    type="text" 
+                    style={{ width: '90%', padding: '16px', margin: '16px', fontSize: '1.5rem' }} 
+                    onChange={searchHandler}
+                    value={searchQuery}
+                    placeholder="Search movie..."
+                />
+                <button style={{ position: 'absolute', top: '36px', right: '64px' }} onClick={resetSearchHandler}>X</button>
+            </div>
+
+            <div style={{ display: 'flex', gap: '20px', overflow: 'scroll', width: '100%' }}>
+                {movies.map((mov, i) => (
+                    <MovieCard 
+                        movie={mov}
+                        key={Math.random()}
+                        ranking={i+1}
+                        onDelete={() => onDeleteHandler(i)}
+                    />
+                ))}
+            </div>
+
+            <button onClick={() => console.log(searchQuery)}>Show search query</button>
+        </div>
     )
 }
--- todo-app/todo-react/src/components/MovieCard.jsx
+++ todo-app/todo-react/src/components/MovieCard.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export default function MovieCard(props) {
-    const { movie: mv } = props;
+    const { movie: mv, ranking, onDelete } = props;
+
+    // inner state: every state is separated
+    const [clicked, setClicked] = useState(false);
+
+    // let clicked = false;
     
     return (
-        <div style={{ border: '1px solid red' }}>
+        <div 
+            style={{ border: `2px solid ${clicked ? 'red' : 'black'}`, position: 'relative', padding: '8px' }} 
+            onClick={() => setClicked(!clicked)}
+        >
+            <button 
+                style={{ position: 'absolute', top: '5px', right: '5px' }}
+                onClick={onDelete}
+            >
+                x
+            </button>
             <img src={mv.poster} alt="" width={'200px'} />
-            <p>{mv.title}</p>
+            <p>{ranking}. {mv.title}</p>
             <p>genre</p>
             <p>{props.button}</p>
         </div>
